09c3c9ec20bf4769fe412b168b64e11367f0a5d1
aynazar-sylyyew-dev <sylyyewsylyyew5@gmail.com>
2026-09-12 13:30:04 +0500

    Update the file configuration

--- core/domain/src/main/java/com/blueplayer/core/domain/repository/TrackRepository.kt
+++ core/domain/src/main/java/com/blueplayer/core/domain/repository/TrackRepository.kt
@@ -2,9 +2,14 @@ package com.blueplayer.core.domain.repository
 
 import com.blueplayer.core.domain.model.Track
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 interface TrackRepository {
+    val tracks: StateFlow<List<Track>>
+
     suspend fun getTracks(): List<Track>
     suspend fun searchTracks(query: String): List<Track>
     fun observeChanges(): Flow<Long>
+
+    suspend fun rescan()
 }
